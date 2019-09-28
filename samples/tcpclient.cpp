1b1310b4a38e3fd34004983d3a5eea346c8f2614
TimmyCheng <timmy_cheng@foxmail.com>
2019-09-28 20:33:18 +0800

    1.test修改为samples

---
+++ samples/tcpclient.cpp
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "c++/tcp.h"
+
+int main(int argc, char** argv)
+{
+	ModuleTcp client(8080, "192.168.3.217");
+	client.connectToServer();
+	
+	while(1)
+	{
+		char buf[1024] = "";
+		client.readFrom((char*)buf);
+		printf("%s\n");
+		usleep(100000);
+	}
+	return 0;
+}
